176f5c7b1bf91d95a3014fbd5ee19524a27fbbcc
CarIosLopez <contacto.carloseduardolopez@gmail.com>
2022-06-08 23:53:35 -0500

    Lists
    Exercise of lists

---
+++ Lists/names.py
@@ -0,0 +1,32 @@
+# Store the names of a few of your friends in a list called names. 
+# Print each person’s name by accessing each element in the list, one at a time. 
+
+name_friends = ["Camilo","Mario","Petro", "AMLO"]
+print(name_friends[0].title())
+print(name_friends[1].title())
+print(name_friends[2].title())
+print(name_friends[3].title())
+
+
+
+# Start with the list you used, but instead of just printing each person’s name,
+# print a message to them. The text of each message should be the same, but each message 
+# should be personalized with the person’s name.
+
+print(name_friends[0].title() + " isn't my friend")
+print(name_friends[1].title() + " isn't my friend")
+print(name_friends[2].title() + " isn't my friend")
+print(name_friends[3].title() + " isn't my friend")
+
+# Think of your favorite mode of transportation, such as a motorcycle or a car, 
+# and make a list that stores several examples. Use your list to print a series 
+# of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+i_want = ["Travel", "Health", "a Motorcycle", "learn too much of programming", "change the world"]
+print(f"I want {i_want[0].lower()}")
+print(f"I want {i_want[1].lower()}")
+print(f"I want {i_want[2].lower()}")
+print(f"I want {i_want[3].lower()}")
+print(f"I want {i_want[4].lower()}")
+
+
